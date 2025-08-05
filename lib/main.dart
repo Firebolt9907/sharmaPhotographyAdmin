029ddf7d899588b1856a6aaf70f76e55702803e6
Rishu Sharma <rishushar99@gmail.com>
2025-08-05 17:46:27 -0500

    tried to add location access, need to test on physical device

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 import 'package:sharma_photography_admin/enums.dart';
 import 'package:sharma_photography_admin/imageUpload.dart';
+import 'package:image/image.dart' as img;
 
 void main() {
   runApp(const MyApp());
@@ -45,6 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
             TextField(
               controller: tokenController,
               decoration: InputDecoration(labelText: "Github Token"),
+              obscureText: true,
             ),
             ElevatedButton(
               onPressed: () async {
@@ -52,6 +58,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   source: ImageSource.gallery,
                 );
                 if (pickedFile != null) {
+                  var location = await getLocation(pickedFile.path);
+                  print("Location: $location");
                   setState(() {});
                 }
               },
@@ -68,12 +76,28 @@ class _MyHomePageState extends State<MyHomePage> {
               controller: descController,
               decoration: InputDecoration(labelText: "Description"),
             ),
-            ElevatedButton(onPressed: () {
-              uploadImage(tokenController.text, pickedFile.path, descController.text, ImageType.photo);
-            }, child: Text("Add new Photo")),
-            ElevatedButton(onPressed: () {
-              uploadImage(tokenController.text, pickedFile.path, descController.text, ImageType.painting);
-            }, child: Text("Add new Painting")),
+            ElevatedButton(
+              onPressed: () {
+                uploadImage(
+                  tokenController.text,
+                  pickedFile.path,
+                  descController.text,
+                  ImageType.photo,
+                );
+              },
+              child: Text("Add new Photo"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                uploadImage(
+                  tokenController.text,
+                  pickedFile.path,
+                  descController.text,
+                  ImageType.painting,
+                );
+              },
+              child: Text("Add new Painting"),
+            ),
           ],
         ),
       ),
@@ -81,3 +105,40 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+getLocation(String imagePath) async {
+  final bytes = await File(imagePath).readAsBytes();
+  final image = img.decodeImage(bytes);
+  if (image == null || image.exif.gpsIfd.data.isEmpty) {
+    throw Exception("No GPS data found in image");
+  }
+  var longitude = latLonDecode(image.exif.gpsIfd.data[4]);
+  var latitude = latLonDecode(image.exif.gpsIfd.data[2]);
+  print(
+    "https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1",
+  );
+  http
+      .get(
+        Uri.parse(
+          'https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1',
+        ),
+      )
+      .then((response) {
+        if (response.statusCode == 200) {
+          print("city: ${response.body}");
+        } else {
+          print(response.body);
+          print(response.statusCode);
+          print('Failed to get location');
+        }
+      });
+  return "No location found";
+}
+
+latLonDecode(var longitudeOrLatitude) {
+  var str = longitudeOrLatitude.toString();
+  str = str.replaceAll("/1,", ",");
+  var degrees = double.parse(str.split(", ")[0].replaceAll("[", ""));
+  var minutes = double.parse(str.split(", ")[1]);
+  var seconds = double.parse(str.split(", ")[2].replaceAll("/100]", "")) / 100;
+  return (degrees + (minutes / 60) + (seconds / 3600)).toStringAsFixed(8);
+}
